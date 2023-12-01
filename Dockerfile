f7bdfee753d10193219fcd2d2f71e4aae18e4505
ellrobot <amprivat2@gmail.com>
2023-12-01 12:30:02 +0900

    seterah

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+######## Kynan #######
+
+FROM kynan1503/naya-userbot:main
+
+
+COPY installer.sh .
+
+RUN bash installer.sh
+
+# changing workdir
+WORKDIR "/root/naya1503"
+
+# start the bot.
+CMD ["bash", "start"]
---
+++ modules/__init__.py
@@ -0,0 +1,282 @@
+# Ultroid - UserBot
+# Copyright (C) 2021-2023 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+
+import asyncio
+import hashlib
+import os
+import os.path
+import re
+import shlex
+import time
+import traceback
+from os.path import basename
+from random import choice
+from time import time
+from traceback import format_exc
+from typing import Optional, Tuple, Union
+from urllib.request import urlretrieve
+
+import aiofiles
+import aiohttp
+import requests
+from Ayra import *
+from Ayra._misc import owner_and_sudos, sudoers
+from Ayra._misc._assistant import asst_cmd, callback, in_pattern
+from Ayra._misc._decorators import ayra_cmd, compile_pattern
+from Ayra._misc._wrappers import eod, eor
+from Ayra.configs import Var
+from Ayra.dB import AYRA_IMAGES, DEVS
+from Ayra.fns.admins import admin_check
+from Ayra.fns.helper import *
+from Ayra.fns.info import *
+from Ayra.fns.misc import *
+from Ayra.fns.tools import *
+from Ayra.fns.ytdl import get_videos_link
+from Ayra.kynan import *
+from Ayra.version import __version__ as AyraVer
+from Ayra.version import ayra_version
+from pytgcalls import GroupCallFactory
+from pytgcalls.exceptions import GroupCallNotFoundError
+from requests import get
+from telethon import Button, events
+from telethon.tl import functions, types  # pylint:ignore
+from telethon.utils import get_display_name
+
+from strings import get_help, get_string
+
+from .database.strg import Storage
+
+Redis = udB.get_key
+con = TgConverter
+quotly = Quotly()
+OWNER_NAME = ayra_bot.full_name
+OWNER_ID = ayra_bot.uid
+
+LOG_CHANNEL = udB.get_key("LOG_CHANNEL")
+
+
+def STORAGE(n):
+    return Storage(Path("data") / n)
+
+
+"""
+def inline_pic():
+    INLINE_PIC = udB.get_key("INLINE_PIC")
+    if INLINE_PIC is None:
+        INLINE_PIC = choice(AYRA_IMAGES)
+    elif INLINE_PIC == False:
+        INLINE_PIC = None
+    return INLINE_PIC
+"""
+
+Telegraph = telegraph_client()
+
+List = []
+Dict = {}
+InlinePlugin = {}
+N = 0
+cmd = ayra_cmd
+STUFF = {}
+
+# Chats, which needs to be ignore for some cases
+# Considerably, there can be many
+# Feel Free to Add Any other...
+
+NOSPAM_CHAT = [
+    -1002062073372
+]
+
+
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    """run command in terminal"""
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+
+
+KANGING_STR = [
+    "Plagiat hehe...",
+    "Ijin bre comot stiker ini ke paket saya...",
+    "Boleh juga nih...",
+    "Keren juga nih...",
+    "Lah apaan tuh...",
+]
+
+ATRA_COL = [
+    "DarkCyan",
+    "DeepSkyBlue",
+    "DarkTurquoise",
+    "Cyan",
+    "LightSkyBlue",
+    "Turquoise",
+    "MediumVioletRed",
+    "Aquamarine",
+    "Lightcyan",
+    "Azure",
+    "Moccasin",
+    "PowderBlue",
+]
+
+all_col = [
+    "Black",
+    "Navy",
+    "DarkBlue",
+    "MediumBlue",
+    "Blue",
+    "DarkGreen",
+    "Green",
+    "Teal",
+    "DarkCyan",
+    "DeepSkyBlue",
+    "DarkTurquoise",
+    "MediumSpringGreen",
+    "Lime",
+    "SpringGreen",
+    "Aqua",
+    "Cyan",
+    "MidnightBlue",
+    "DodgerBlue",
+    "LightSeaGreen",
+    "ForestGreen",
+    "SeaGreen",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "LimeGreen",
+    "MediumSeaGreen",
+    "Turquoise",
+    "RoyalBlue",
+    "SteelBlue",
+    "DarkSlateBlue",
+    "MediumTurquoise",
+    "Indigo  ",
+    "DarkOliveGreen",
+    "CadetBlue",
+    "CornflowerBlue",
+    "RebeccaPurple",
+    "MediumAquaMarine",
+    "DimGray",
+    "DimGrey",
+    "SlateBlue",
+    "OliveDrab",
+    "SlateGray",
+    "SlateGrey",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "MediumSlateBlue",
+    "LawnGreen",
+    "Chartreuse",
+    "Aquamarine",
+    "Maroon",
+    "Purple",
+    "Olive",
+    "Gray",
+    "Grey",
+    "SkyBlue",
+    "LightSkyBlue",
+    "BlueViolet",
+    "DarkRed",
+    "DarkMagenta",
+    "SaddleBrown",
+    "DarkSeaGreen",
+    "LightGreen",
+    "MediumPurple",
+    "DarkViolet",
+    "PaleGreen",
+    "DarkOrchid",
+    "YellowGreen",
+    "Sienna",
+    "Brown",
+    "DarkGray",
+    "DarkGrey",
+    "LightBlue",
+    "GreenYellow",
+    "PaleTurquoise",
+    "LightSteelBlue",
+    "PowderBlue",
+    "FireBrick",
+    "DarkGoldenRod",
+    "MediumOrchid",
+    "RosyBrown",
+    "DarkKhaki",
+    "Silver",
+    "MediumVioletRed",
+    "IndianRed ",
+    "Peru",
+    "Chocolate",
+    "Tan",
+    "LightGray",
+    "LightGrey",
+    "Thistle",
+    "Orchid",
+    "GoldenRod",
+    "PaleVioletRed",
+    "Crimson",
+    "Gainsboro",
+    "Plum",
+    "BurlyWood",
+    "LightCyan",
+    "Lavender",
+    "DarkSalmon",
+    "Violet",
+    "PaleGoldenRod",
+    "LightCoral",
+    "Khaki",
+    "AliceBlue",
+    "HoneyDew",
+    "Azure",
+    "SandyBrown",
+    "Wheat",
+    "Beige",
+    "WhiteSmoke",
+    "MintCream",
+    "GhostWhite",
+    "Salmon",
+    "AntiqueWhite",
+    "Linen",
+    "LightGoldenRodYellow",
+    "OldLace",
+    "Red",
+    "Fuchsia",
+    "Magenta",
+    "DeepPink",
+    "OrangeRed",
+    "Tomato",
+    "HotPink",
+    "Coral",
+    "DarkOrange",
+    "LightSalmon",
+    "Orange",
+    "LightPink",
+    "Pink",
+    "Gold",
+    "PeachPuff",
+    "NavajoWhite",
+    "Moccasin",
+    "Bisque",
+    "MistyRose",
+    "BlanchedAlmond",
+    "PapayaWhip",
+    "LavenderBlush",
+    "SeaShell",
+    "Cornsilk",
+    "LemonChiffon",
+    "FloralWhite",
+    "Snow",
+    "Yellow",
+    "LightYellow",
+    "Ivory",
+    "White",
+]
